395cf53c534bdd54189940209fc78b50c571a52e
mariari <mariari@protonmail.ch>
2025-06-04 01:02:39 +0800

    Make the initial View Genserver
    This offering is incomplete, but we can:
    
    1. add_views
    
    Further we add it to the startup supervisor

---
+++ lib/gt_bridge/views.ex
@@ -0,0 +1,55 @@
+defmodule GtBridge.Views do
+  @moduledoc """
+  I hold views for various types, please query me to find out views you are interested in.
+  """
+  use GenServer
+  use TypedStruct
+
+  typedstruct do
+    field(:mapping, %{atom() => MapSet.t(code())}, default: Map.new())
+  end
+
+  @type options() :: {:name, GenServer.name()}
+  @type code() :: {module(), function_name :: atom()}
+
+  @spec start_link([options()]) :: GenServer.on_start()
+  def start_link(init_args) do
+    name = Keyword.get(init_args, :name, nil)
+    GenServer.start_link(__MODULE__, init_args, name: name)
+  end
+
+  @impl true
+  def init(_init_args) do
+    {:ok, %__MODULE__{}}
+  end
+
+  ############################################################
+  #                      Public RPC API                      #
+  ############################################################
+
+  @spec add(GenServer.server(), atom(), code()) :: :ok
+  def add(server, module, code) do
+    GenServer.cast(server, {:add, module, code})
+  end
+
+  ############################################################
+  #                    Genserver Behavior                    #
+  ############################################################
+
+  @impl true
+  def handle_cast({:add, module, code}, state) do
+    {:noreply, handle_add_view(module, code, state)}
+  end
+
+  ############################################################
+  #                 Genserver Implementation                 #
+  ############################################################
+
+  @spec handle_add_view(atom(), code(), t()) :: t()
+  def handle_add_view(module, code, state = %__MODULE__{}) do
+    new_mapping =
+      Map.update(state.mapping, module, MapSet.new([code]), &MapSet.put(&1, code))
+
+    %__MODULE__{state | mapping: new_mapping}
+  end
+end
