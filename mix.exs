0a7f7b35a59ed397bd96105878ac44795923b7c4
mariari <mariari@protonmail.com>
2025-10-14 12:37:00 +0800

    V0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GtBridge.MixProject do
   def project do
     [
       app: :gt_bridge,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       plt_add_apps: [:mix, :ex_unit],
