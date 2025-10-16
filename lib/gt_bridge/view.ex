9583a4c6f6235a5dca9b99e22c15c188d60a4f30
mariari <mariari@protonmail.com>
2025-10-16 16:23:27 +0800

    Add a macro to determine the argument type from the input arguments

--- lib/gt_bridge/view.ex
+++ lib/gt_bridge/view.ex
@@ -96,4 +96,100 @@ defmodule GtBridge.View do
       apply(view_module, :as_dict, [view_result])
     end)
   end
+
+  ############################################################
+  #               Helpers For Determining Module             #
+  ############################################################
+
+  @spec match_view_argument_to_module(Macro.t(), Macro.Env.t()) :: module()
+  def match_view_argument_to_module(args, env) do
+    x = GtBridge.View.match_view_argument(args, env)
+
+    if is_nil(x) do
+      quote do
+        __MODULE__
+      end
+    else
+      x
+    end
+  end
+
+  @doc """
+  I handle matching arguments to resolve the module for `defview/1`.
+
+  Most data structures will resolve however there are a few exceptions:
+
+  1. 3 tuples, are not supported as it overlaps with `Macro.t/0`.
+  2. Atoms are not supports, as it overlaps with `x = y`
+  """
+  @spec match_view_argument(Macro.t(), Macro.Env.t()) :: module() | nil
+  def match_view_argument([{:\\, _, [arg_l, _]} | args], caller) do
+    match_view_argument([arg_l, args], caller)
+  end
+
+  def match_view_argument([{:=, _, [arg_l, arg_r]} | args], caller) do
+    match_view_argument([determine_constraining_arg(arg_l, arg_r), args], caller)
+  end
+
+  def match_view_argument([arg | _args], caller) do
+    match_arg(arg, caller)
+  end
+
+  # TODO Make this more robust
+  @spec determine_constraining_arg(Macro.t(), Macro.t()) :: Macro.t()
+  def determine_constraining_arg({atom, _, _}, arg2) when is_atom(atom) do
+    arg2
+  end
+
+  def determine_constraining_arg(arg1, _), do: arg1
+
+  @spec match_arg(Macro.t(), Macro.Env.t()) :: module() | nil
+  def match_arg({:%, _, [{:__aliases__, [alias: false], atoms} | _]}, _),
+    do: Enum.reduce(atoms, &Module.concat/2)
+
+  def match_arg({:%, _, [{:__MODULE__, _, _} | _]}, _), do: nil
+  def match_arg({:%, _, [{:__aliases__, [alias: mod], _} | _]}, _), do: mod
+  def match_arg({:%{}, _, keyword}, _), do: Keyword.get(keyword, :__struct__, Map)
+
+  def match_arg(x, caller) when is_tuple(x) do
+    expanded = Macro.expand(x, caller)
+
+    if x == expanded do
+      case x do
+        {value, _, _} ->
+          match_arg(value, caller)
+
+        # If it's not a 3 tuple likely actually a tuple view
+        _ ->
+          Tuple
+      end
+    else
+      match_arg(expanded, caller)
+    end
+  end
+
+  def match_arg(x, _) when is_list(x) do
+    List
+  end
+
+  def match_arg(x, _) when is_pid(x) do
+    Pid
+  end
+
+  # Don't handle atom, x = y. Fallback on failure
+  def match_arg(x, _) when is_atom(x) do
+    nil
+  end
+
+  # These types don't get resolved to proxy objects currently
+
+  def match_arg({:<<>>, _, _}, _), do: Binary
+
+  def match_arg(x, _) when is_integer(x) do
+    Integer
+  end
+
+  def match_arg(x, _) when is_float(x) do
+    Float
+  end
 end
